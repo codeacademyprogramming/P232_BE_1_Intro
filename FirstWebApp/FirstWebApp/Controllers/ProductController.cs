5202c20a7655b6a66e83a6900a98836af10420a2
eaghayev <eaghayevv@gmail.com>
2023-03-28 18:08:57 +0400

    project added

---
+++ FirstWebApp/FirstWebApp/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace FirstWebApp.Controllers
+{
+    public class ProductController:Controller
+    {
+        //public ContentResult GetInfo()
+        //{
+        //    ContentResult result = new ContentResult();
+        //    result.Content = "Product info";
+        //    return result;
+        //}
+
+        //public JsonResult GetInfo()
+        //{
+        //    JsonResult result = new JsonResult(new { Name = "Iphone", Price = 45 });
+        //    return result;
+        //}
+
+        public ViewResult Detail()
+        {
+            //ViewResult result = new ViewResult();
+            //result.ViewName = "detail";
+            //return result;
+
+            return View();
+        }
+
+        //public ContentResult GetInfo(string brand,string category,decimal price)
+        //{
+        //    ContentResult result = new ContentResult();
+        //    result.Content = "Product info - brand: " + brand + " - category: " + category+" - price:"+price;
+        //    return result;
+        //}
+
+        public JsonResult GetInfo(string brand,string category,decimal price)
+        {
+            //JsonResult result = new JsonResult(new { brand = brand, category = category,salePrice=price });
+            //return result;
+
+            return Json(new { brand = brand, category = category, salePrice = price });
+        }
+    }
+}
